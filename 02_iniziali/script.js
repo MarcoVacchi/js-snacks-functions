1dde97502ee89d4ef826a565b18ef25adc3893ac
MarcoVacchi <mvacchi816@gmail.com>
2025-03-03 14:47:19 +0100

    complete exercise 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const firstLetter = () => {
+
+let arrResult = []
+
+for (let i = 0; i < names.length; i++) {
+    result = names[i];
+    arrResult.push(result[0])
+    
+}
+    return arrResult;
+} 
+
+
+// devo creare un array che sostituisca il vecchio array prendendo solo le iniziali di ogni parola del primo array
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(firstLetter());
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]

8af8a5d1f4b57c20955e48af8f2b84b198f21678
MarcoVacchi <mvacchi816@gmail.com>
2025-03-03 14:04:42 +0100

    bonus ex 1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,14 +1,21 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = prompt('inserisci un nome utente');
+//const userName = prompt('inserisci un nome utente');
+const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
 
-const myFunct = (saluto) => { 
+    const myFunct = (saluto) => { 
     saluto = `ciao ${userName}`
     return saluto;
-}
+} 
+
+/*
+    function myFunct (saluto) { 
+    saluto = `ciao ${userName}`
+    return saluto;
+} /*
 
 // dichiaro la mia funzione che verificherà che venga inserita una stringa, e restituisca un saluto
 

4877ec92849a758d5828872bbfeb9a1ceca2f1bc
MarcoVacchi <mvacchi816@gmail.com>
2025-03-03 14:02:35 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,21 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = prompt('inserisci un nome utente');
+
+
+// Dichiara la funzione qui.
+
+const myFunct = (saluto) => { 
+    saluto = `ciao ${userName}`
+    return saluto;
+}
+
+// dichiaro la mia funzione che verificherà che venga inserita una stringa, e restituisca un saluto
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(myFunct(userName));
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario

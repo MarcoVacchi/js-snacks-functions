7a2ca1e8616fe1aff0710d99b77cfb84263a5679
MarcoVacchi <mvacchi816@gmail.com>
2025-03-03 16:22:13 +0100

    feat complete exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,8 +3,20 @@
 const word = 'javascript';
 
 
-// Dichiara la funzione qui.
+//Dichiara la funzione qui.
+const vocal = (param) => {
+let count = 0;
+for (let i = 0; i < param.length; i++) {
+let result = param[i];
+if (result === 'a'|| result === 'e' || result ==='i' || result === 'o' || result === 'u' || result === 'A'|| result === 'E' || result ==='I' || result === 'O' || result === 'U'){
+    count ++;
+}
+}
+    return count;
+}
 
+vocal(word)
+console.log(vocal(word))
 
 // Invoca la funzione qui e stampa il risultato in console
 
